9bd42c0d814f93ec53d670229bd7ef4adf868f47
unknown <mishra.saumitra06@gmail.com>
2021-09-02 22:52:23 +0530

    Created sign-in, sign-up page, database schema and set up the library for using cookies

--- controllers/users_Controller.js
+++ controllers/users_Controller.js
@@ -1,5 +1,28 @@
 module.exports.profile = function(req,res){
     return res.render('users',{
         title: "Users"
+    });
+};
+//Render the sign up page
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title: "Codial | Sign Up"
+    });
+};
+
+//Render the sign in page
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title: "Codial | Sign In"
     })
+}
+
+//get the sign up data
+module.exports.create = function(req,res){
+    //TODO Later
+}
+
+
+module.exports.createSession = function(req,res){
+    //TODO Later
 }
