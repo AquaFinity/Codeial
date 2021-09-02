83771b59c8a80aebcdcc8f3b0200a3befc532026
unknown <mishra.saumitra06@gmail.com>
2021-09-03 00:54:32 +0530

    developed the sign-up function

--- controllers/users_Controller.js
+++ controllers/users_Controller.js
@@ -1,3 +1,4 @@
+const User = require('../models/user');
 module.exports.profile = function(req,res){
     return res.render('users',{
         title: "Users"
@@ -19,7 +20,28 @@ module.exports.signIn = function(req,res){
 
 //get the sign up data
 module.exports.create = function(req,res){
-    //TODO Later
+    if(req.body.password!= req.body.confirm_password){
+        res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email},function(err,user){
+        if(err){
+            console.log('Error in finding user in signing Up');
+            return;
+        }
+        if(!user){
+            User.create(req.body,function(err,user){
+                if(err){
+                    console.log('Error in creating user while signing Up');
+                    return;
+                }
+                return res.redirect('/users/sign-in');
+            });
+        }
+        else{
+            return res.redirect('back');
+        }
+    });
 }
 
 
