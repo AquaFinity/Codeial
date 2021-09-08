23177b2bba699cc215b541e184b922675d715f05
unknown <mishra.saumitra06@gmail.com>
2021-09-08 17:40:35 +0530

    Added comments under post and displayed the comments on the home page under the post it belongs to

--- controllers/home_Controller.js
+++ controllers/home_Controller.js
@@ -11,7 +11,14 @@ module.exports.home = function(req,res){
     });*/
 
     // populate the user of each post
-    Post.find({}).populate('user').exec(function(err,posts){
+    Post.find({})
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .populate('user').exec(function(err,posts){
         return res.render('home',{
             title: "Codeial Home",
             posts: posts
